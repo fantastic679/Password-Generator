64058df4117762e589a28814880d2cf09dcb62f8
Steven Phan <stevenphan@msn.com>
2021-12-16 03:33:00 +1100

    Cleaned up structure of .js, added comments

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-// Assignment Code
+// Trigger password generation on clicking button
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
+  // Prompt for password criteria
+  // WHEN I click the button to generate a password
+  // THEN I am presented with a series of prompts for password criteria
+  // WHEN prompted for password criteria
+  // THEN I select which criteria to include in the password
+
+  // Generate password
+  var password = generatePassword();
+
+  // Display the password
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Generate password
+function generatePassword() {
+// length (8 - 128)
+// lowercase, uppercase, numeric, and/or special charcters
+// verify input - at least one character type should be selected
+  return "PLACEHOLDER PASSWORD";
+}
+
