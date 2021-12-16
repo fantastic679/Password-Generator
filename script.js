dbfddc9e6c1f6eb1f206c703f1890e62df13015f
Steven Phan <stevenphan@msn.com>
2021-12-16 13:24:29 +1100

    Completed code for verifying user input, password generation portion still to be coded

--- script.js
+++ script.js
@@ -22,9 +22,64 @@ function writePassword() {
 
 // Generate password
 function generatePassword() {
-// length (8 - 128)
-// lowercase, uppercase, numeric, and/or special charcters
-// verify input - at least one character type should be selected
-  return "PLACEHOLDER PASSWORD";
+verifyLength();
+verifyCharacters();
+return "PLACEHOLDER PASSWORD";
+}
+
+// accepts numeric password lengths between 8 and 128 inclusive
+// if doesn't meet these conditions, alerts user and makes them re-enter
+function verifyLength() {
+  var numericOnly  = /^[0-9]+$/; // use Regular Expressions to ensure 0-9 characters only
+                                 // ^ = start of string
+                                 // [0-9]= numeric characters 0-9
+                                 // + = at least one character
+                                 // $ = end of string
+  var passwordLength = prompt("Please enter numeric value for the password length", "Only lengths between 8 and 128 inclusive accepted.");
+  if (passwordLength.match(numericOnly) != null && passwordLength >= 8 && passwordLength <= 128) {
+    alert("GOOD PASSWORD LENGTH");
+  } else {
+    alert("Only lengths between 8 and 128 inclusive accepted.");
+    verifyLength();
+    return;
+  }
+}
+
+// prompts users to choose if they want lowercase, uppercase, numeric, and/or special charcters
+// if nothing chosen,  alerts user and makes them choose again
+function verifyCharacters() {
+  var includeLowerCase;
+  if (window.confirm("Press OK to include at least one lower case character: abc...")) {
+    includeLowerCase = true;
+  } else {
+    includeLowerCase = false;
+  }
+
+  var includeUpperCase;
+  if (window.confirm("Press OK to include at least one upper case character: ABC...")) {
+    includeUpperCase = true;
+  } else {
+    includeUpperCase = false;
+  }
+
+  var includeNumeric;
+  if (window.confirm("Press OK to include at least one numeric character: 123...")) {
+    includeNumeric = true;
+  } else {
+    includeNumeric = false;
+  }
+
+  var includeSpecial;
+  if (window.confirm("Press OK to include at least one special character: !@#...")) {
+    includeSpecial = true;
+  } else {
+    includeSpecial = false;
+  }
+
+  if (includeLowerCase === false && includeUpperCase === false && includeNumeric === false && includeSpecial === false) {
+    alert("Please select at least one type of character.");
+    verifyCharacters();
+    return;
+  }
 }
 
